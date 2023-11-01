67b866eac55f829361981d7b4a3d2d36d59414d8
LMCuber <leo.bozkir@outlook.com>
2023-11-01 13:01:42 +0100

    push

--- aeros.py
+++ aeros.py
@@ -10,7 +10,8 @@ vertices = []
 
 for _ in range(8):
     vertices.append([randf(-1, 1), randf(-1, 1), randf(-1, 1)])
-
+y = 0
+g = 0.1
 m = 1
 base_color = lambda: rand_rgba()
 outline_color = (0, 0, 0, 255)
@@ -30,25 +31,36 @@ plane = Crystal(
     ], [
 
     ], [
-        [[base_color(), outline_color], 0, 1, 2, 3],
-        [[base_color(), outline_color], 4, 5, 1, 0],
-        [[base_color(), outline_color], 4, 5, 6, 7],
-        [[base_color(), outline_color], 7, 6, 2, 3],
-        [[base_color(), outline_color], 4, 0, 3, 7],
-        [[base_color(), outline_color], 1, 5, 6, 2]
+        [[base_color(), outline_color], rand(0, 7), rand(0, 7), rand(0, 7)],
+        [[base_color(), outline_color], rand(0, 7), rand(0, 7), rand(0, 7)],
+        [[base_color(), outline_color], rand(0, 7), rand(0, 7), rand(0, 7)],
+        [[base_color(), outline_color], rand(0, 7), rand(0, 7), rand(0, 7)],
+        [[base_color(), outline_color], rand(0, 7), rand(0, 7), rand(0, 7)],
+        [[base_color(), outline_color], rand(0, 7), rand(0, 7), rand(0, 7)],
+        [[base_color(), outline_color], rand(0, 7), rand(0, 7), rand(0, 7)],
+        [[base_color(), outline_color], rand(0, 7), rand(0, 7), rand(0, 7)],
+        [[base_color(), outline_color], rand(0, 7), rand(0, 7), rand(0, 7)],
     ],
-    (WIDTH / 2, HEIGHT / 2), 200, 0, 0, 0, 0.015, 0.015, 0.015,
+    (WIDTH / 2, HEIGHT / 2), 200, 0, 0, 0, 0, 0, 0,
     fill_as_connections=False
 )
 
 clock = pygame.time.Clock()
 running = True
 while running:
+    mouses = pygame.mouse.get_pressed()
     clock.tick(120)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        elif event.type == pygame.MOUSEMOTION:
+            if mouses[0]:
+                dx, dy = event.rel
+                m = 0.015
+                plane.ya -= dx * 0.01
+                plane.xa += dy * 0.01
 
     fill_rect(REN, (230, 230, 230, 255), (0, 0, WIDTH, HEIGHT))
     plane.update()
