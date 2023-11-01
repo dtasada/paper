e357632c17e0d3f64b7432ef5497f48f576ecaca
LMCuber <leo.bozkir@outlook.com>
2023-11-01 12:52:25 +0100

    changes

--- aeros.py
+++ aeros.py
@@ -5,32 +5,39 @@ WIDTH, HEIGHT = 800, 600
 WIN = Window(size=(WIDTH, HEIGHT), title="PAPER")
 REN = Renderer(WIN)
 
-m = 200
-base_color = (140, 140, 140, 255)
+
+vertices = []
+
+for _ in range(8):
+    vertices.append([randf(-1, 1), randf(-1, 1), randf(-1, 1)])
+
+m = 1
+base_color = lambda: rand_rgba()
 outline_color = (0, 0, 0, 255)
 plane = Crystal(
     REN, [
-        [-m, -m, m],
-        [m, -m, m],
-        [m, m, m],
-        [-m, m, m],
-        [-m, -m, -m],
-        [m, -m, -m],
-        [m, m, -m],
-        [-m, m, -m],
-    ], [
-        
+        # [-m, -m, m],
+        # [m, -m, m],
+        # [m, m, m],
+        # [-m, m, m],
+        # [-m, -m, -m],
+        # [m, -m, -m],
+        # [m, m, -m],
+        # [-m, m, -m],
+        *vertices
     ], [
 
     ], [
-        [[base_color, outline_color], 0, 1, 2, 3],
-        [[base_color, outline_color], 4, 5, 1, 0],
-        [[base_color, outline_color], 4, 5, 6, 7],
-        [[base_color, outline_color], 7, 6, 2, 3],
-        [[base_color, outline_color], 4, 0, 3, 7],
-        [[base_color, outline_color], 1, 5, 6, 2]
+
+    ], [
+        [[base_color(), outline_color], 0, 1, 2, 3],
+        [[base_color(), outline_color], 4, 5, 1, 0],
+        [[base_color(), outline_color], 4, 5, 6, 7],
+        [[base_color(), outline_color], 7, 6, 2, 3],
+        [[base_color(), outline_color], 4, 0, 3, 7],
+        [[base_color(), outline_color], 1, 5, 6, 2]
     ],
-    (WIDTH / 2, HEIGHT / 2), 1, 0, 0, 0, 0.015, 0.015, 0.015,
+    (WIDTH / 2, HEIGHT / 2), 200, 0, 0, 0, 0.015, 0.015, 0.015,
     fill_as_connections=False
 )
 
