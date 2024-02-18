95681433eb0d7ac3542eb99c05c9b5220693e005
Daniel Tasada <daniel.tasada@icloud.com>
2024-02-18 11:47:58 +0100

    idek

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <raylib.h>
 #include <math.h>
+#include <vector>
 
-#define AIR_PRESSURE 101325 // Air pressure at sea level: N/m^2
-#define AIR_DENSITY 1.225 // Air pressure at sea level: kg/m^3
+#include "things.hpp"
+
+#define AIR_PRESSURE 101325 // Air pressure at sea level: N/m^2 #define AIR_DENSITY 1.225 // Air pressure at sea level: kg/m^3
 
 #define LIFT(liftCoef, airDensity, airflowSpeed, wingArea, wingHeight) \
 	liftCoef * airDensity * pow(airflowSpeed, 2) * wingArea * 0.5
@@ -13,61 +15,23 @@
 #define P_AIR(m, v) m * v
 #define REYNOLDS(p, v, D, frictionCoef) (p * v * D) / frictionCoef
 
-struct Plane {
-    // Physical properties
-    double mass;
-    double wingspan;
-    double wingArea;
-    double dragCoefficient;
-    double liftCoefficient;
-    double thrust;
-
-    // State variables
-	double velocity;
-	double altitude;
-	double pitchAngle;
-	double rollAngle;
-	double yawAngle;
-
-    // Aerodynamic forces and moments
-    double liftForce;
-    double dragForce;
-    double thrustForce;
-    double gravityForce;
-
-    // Constructor
-	Plane(double mass, double wingspan, double wingArea, double dragCoef, double liftCoef, double thrust) {
-		this->mass = mass; // kg
-		this->wingspan = wingspan; // m
-		this->wingArea = wingArea; // m^2
-		this->dragCoefficient = dragCoef; // unitless
-		this->liftCoefficient = liftCoef; // unitless
-		this->thrust = thrust; // N
-	}
-
-    // Function to update the state of the airplane based on physics calculations
-    void update(double timeStep) {
-        // Implement physics calculations here
-        // Update velocity, position, forces, and angles based on aerodynamics
-        // ...
-
-        std::cout << "Updating airplane state...\n";
-    }
-};
-
 int main() {
 	InitWindow(800, 600, "Hello there!");
+	SetTargetFPS(60);
 
-	Camera3D camera;
 	Plane plane = Plane(1000, 40, 400, 1000, 1000, 1000);
+	Model model = LoadModel("models/model_1.obj");
 
 	while (!WindowShouldClose()) {
-		BeginMode3D(camera);
 		BeginDrawing();
 
+		ClearBackground(BLACK);
+
+		DrawModel(model, { 0.0, 0.0, 0.0 }, 1.0, WHITE);
+
 		EndDrawing();
-		EndMode3D();
 	}
 
+	CloseWindow();
 	return 0;
 }
