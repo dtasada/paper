e4672353b2208a088eec1ccf260470d03cef9ecf
Daniel Tasada <daniel.tasada@icloud.com>
2024-06-08 18:34:47 +0200

    not working due to double borrow

--- src/engine.rs
+++ src/engine.rs
@@ -3,12 +3,7 @@ use raylib::prelude::*;
 /// # paper-engine
 // asdf
 
-pub struct Particle {
-    pub pos: Vector2,
-    pub vel: Vector2,
-    pub radius: f32,
-    collision_damping: f32,
-}
+type Cell = Vec<Option<Particle>>;
 
 pub static GRAVITY: f32 = 9.81;
 
@@ -22,6 +17,65 @@ pub fn get_sign(number: f32) -> f32 {
     }
 }
 
+
+pub struct Grid {
+    pub width: i32,
+    pub height: i32,
+    pub content: Vec<Vec<Cell>>,
+}
+
+impl Grid {
+    pub fn new() -> Self {
+        let width = 20;
+        let height = 20;
+
+        Self {
+            width,
+            height,
+            content: vec![vec![]],
+        }
+    }
+
+    pub fn find_collisions(&mut self) {
+        for x in 1..self.width {
+            for y in 1..self.height {
+                for dx in -1..=1 {
+                    for dy in -1..=1 {
+                        self.check_collision((x, y), (x + dx, y + dy));
+                    }
+                }
+            }
+        }
+    }
+
+    fn check_collision(&mut self, cell_a_xy: (i32, i32), cell_b_xy: (i32, i32)) {
+        let cell_a = &mut self.content[cell_a_xy.0 as usize][cell_a_xy.1 as usize];
+        let cell_b = &mut self.content[cell_b_xy.0 as usize][cell_b_xy.1 as usize];
+
+        for particle_a_wrapped in cell_a.iter_mut() {
+            for particle_b_wrapped in cell_b.iter_mut() {
+                if !std::ptr::eq(particle_a_wrapped, particle_b_wrapped) {
+                    let a = particle_a_wrapped.as_mut().unwrap();
+                    let b = particle_b_wrapped.as_mut().unwrap();
+
+                    if check_collision_circles(a.pos, a.radius, b.pos, b.radius) {
+                        let compensation_length = a.radius + b.radius - a.pos.distance_to(b.pos);
+                        a.pos -= compensation_length / 2.0;
+                        b.pos += compensation_length / 2.0;
+                    }
+                }
+            }
+        }
+    }
+}
+
+pub struct Particle {
+    pub pos: Vector2,
+    pub vel: Vector2,
+    pub radius: f32,
+    collision_damping: f32,
+}
+
 impl Particle {
     pub fn new(start_pos: Vector2, vel: Vector2, radius: f32) -> Self {
         Self {
@@ -32,8 +86,8 @@ impl Particle {
         }
     }
 
-    pub fn collision(&mut self, dh: &RaylibDrawHandle, bounds: &Rectangle) {
-        let bounds_size: Vector2 =
+    pub fn collision(&mut self, bounds: &Rectangle) {
+        let bounds_size =
             Vector2::new(bounds.x + bounds.width, bounds.y + bounds.height) -
                 Vector2::one() * self.radius;
 
@@ -46,5 +100,19 @@ impl Particle {
             self.pos.y = bounds_size.y * get_sign(self.pos.y);
             self.vel.y *= -1.0 * self.collision_damping;
         }
+    }
+
+    pub fn update(&mut self, dh: &mut RaylibDrawHandle, bounds: &Rectangle) {
+        self.vel.y += GRAVITY;
+
+        self.pos += self.vel * dh.get_frame_time();
+        self.collision(&bounds);
+
+        dh.draw_circle(
+            self.pos.x as i32,
+            self.pos.y as i32,
+            self.radius,
+            Color::LIGHTBLUE,
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() -> Result<(), String> {
     let (mut rl, thread) = raylib::init().size(1280, 720).build();
     rl.set_target_fps(60);
 
-    // let mut particle = Particle::new(Vector2::new(rl.get_screen_width() as f32 / 2.0, rl.get_screen_height() as f32 / 2.0), Vector2::zero(), 50.0);
+    // Box in which particles are allowed
     let mut bounds = Rectangle::new(
         20.0,
         20.0,
@@ -15,14 +15,19 @@ fn main() -> Result<(), String> {
         rl.get_screen_height() as f32 - 40.0,
     );
 
-    let mut particles: Vec<Particle> = vec![];
+    let mut grid = Grid::new();
 
-    for i in 1..=20 {
-        particles.push(Particle::new(
-            Vector2::new(bounds.x + i as f32 * 50.0, bounds.y + 50.0),
-            Vector2::zero(),
-            50.0,
-        ));
+    let mut particles: Vec<Particle> = Vec::new();
+
+    // Generate particles
+    for i in 1..=grid.width {
+        for j in 1..=grid.height {
+            particles.push(Particle::new(
+                Vector2::new(bounds.x + i as f32 * 50.0, bounds.y + j as f32 * 50.0),
+                Vector2::zero(),
+                20.0,
+            ));
+        }
     }
 
     while !rl.window_should_close() {
@@ -30,18 +35,11 @@ fn main() -> Result<(), String> {
         dh.clear_background(Color::BLACK);
 
         for particle in &mut particles {
-            particle.vel.y += GRAVITY;
-
-            particle.pos += particle.vel * dh.get_frame_time();
-            particle.collision(&dh, &bounds);
-
-            dh.draw_circle(
-                particle.pos.x as i32,
-                particle.pos.y as i32,
-                particle.radius,
-                Color::LIGHTBLUE,
-            );
+            particle.update(&mut dh, &bounds);
         }
+
+        grid.find_collisions();
+
         dh.draw_fps(12, 12);
         dh.draw_rectangle_lines_ex(bounds, 1.0, Color::ORANGE);
     }
